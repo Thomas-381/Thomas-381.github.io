8f96ff5fcf9c5fdd735f7ffcdcfa4bf3f629ace1
Reymond thomas <thomreymond38@gmail.com>
2024-10-21 16:51:59 +0200

    Update : Amélioration du portfolio, nettoyage du .html, fix des RS, fix des problemes des themes sombres et clair

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,13 +1,19 @@
-function() {
-    $("*[id='mode-dark']").toggle();
-    $("*[id='mode-light']").toggle();
-  
-    if (document.documentElement.getAttribute('data-theme') == 'dark') {
-      document.documentElement.setAttribute('data-theme', 'light');
-      $("#mode").removeAttr("src").attr("src", "light.png");
-    } else {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      $("#mode").removeAttr("src").attr("src", "dark.png");
-    }
-  }
-  
+$(document).ready(function () {
+  $("html").show();
+
+  $("*[id='mode-dark']").show();
+  $("*[id='mode-light']").hide();
+
+  $("#switch-mode").click(function () {
+      $("*[id='mode-dark']").toggle();
+      $("*[id='mode-light']").toggle();
+
+      if (document.documentElement.getAttribute('data-theme') == 'dark') {
+          document.documentElement.setAttribute('data-theme', 'light');
+          $("#mode").removeAttr("src").attr("src", "light.png");
+      } else {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          $("#mode").removeAttr("src").attr("src", "dark.png");
+      }
+  });
+});
