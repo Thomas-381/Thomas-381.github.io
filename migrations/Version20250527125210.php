d00256665dc2c14f156915f53bfa6cd549732dc3
Reymond thomas <thomreymond38@gmail.com>
2025-05-27 15:23:31 +0200

    Add migration

---
+++ migrations/Version20250527125210.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250527125210 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE langage ADD couleur VARCHAR(16) DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE langage ALTER visuel SET NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE langage DROP couleur
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE langage ALTER visuel DROP NOT NULL
+        SQL);
+    }
+}
